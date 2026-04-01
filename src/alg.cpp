f275552cbb60530d4db7684cca38e64a056f79fb
vians14 <131287803+vians14@users.noreply.github.com>
2026-04-01 15:05:37 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -32,45 +32,6 @@ int countPairs1(int *arr, int len, int value) {
 
 int countPairs2(int *arr, int len, int value) {
     int count = 0;
-    int left = 0;
-    int right = len - 1;
-    
-    while (left < right) {
-        int sum = arr[left] + arr[right];
-        
-        if (sum == value) {
-            if (arr[left] == arr[right]) {
-                int n = right - left + 1;
-                count += n * (n - 1) / 2;
-                break;
-            }
-            int left_val = arr[left];
-            int left_count = 0;
-            while (left <= right && arr[left] == left_val) {
-                left_count++;
-                left++;
-            }
-            int right_val = arr[right];
-            int right_count = 0;
-            while (right >= left && arr[right] == right_val) {
-                right_count++;
-                right--;
-            }
-            count += left_count * right_count;
-        }
-        else if (sum < value) {
-            left++;
-        }
-        else {
-            right--;
-        }
-    }
-    
-    return count;
-}
-
-int countPairs3(int *arr, int len, int value) {
-    int count = 0;
     
     for (int i = 0; i < len; i++) {
         int target = value - arr[i];
@@ -112,3 +73,45 @@ int countPairs3(int *arr, int len, int value) {
     
     return count;
 }
+
+int countPairs3(int *arr, int len, int value) {
+    int count = 0;
+    int left = 0;
+    int right = len - 1;
+    
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+        
+        if (sum == value) {
+            if (arr[left] == arr[right]) {
+                int n = right - left + 1;
+                count += n * (n - 1) / 2;
+                break;
+            }
+            int left_val = arr[left];
+            int left_count = 0;
+            while (left <= right && arr[left] == left_val) {
+                left_count++;
+                left++;
+            }
+            int right_val = arr[right];
+            int right_count = 0;
+            while (right >= left && arr[right] == right_val) {
+                right_count++;
+                right--;
+            }
+            count += left_count * right_count;
+        }
+        else if (sum < value) {
+            left++;
+        }
+        else {
+            right--;
+        }
+    }
+    
+    return count;
+}
+
+
+
